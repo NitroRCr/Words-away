c73a685f3d18c6c083d14d813e0e062b30ad16b7
NitroRCr <havechicken666@gmail.com>
2020-04-17 10:15:25 +0800

    代码重构

--- js/index.js
+++ js/index.js
@@ -1,11 +1,8 @@
-$('.start-mixin').click(function () {
-    var text = $('#textin').val();
-    var mixin = '\u200b';
+function WordsAway() {}
+WordsAway.prototype.mixin = function (text, mixin = '\u200b', missBrackets = true) {
     var result = '';
     var inBrackets = false;
-    var missBrackets = $('#miss-brackets')[0].checked;
     if (missBrackets) {
-        text = text.replace(/(http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?)/g, '[$1]');
         for (let i in text) {
             let x = text[i];
             if (inBrackets) {
@@ -24,50 +21,62 @@ $('.start-mixin').click(function () {
             result += (mixin + text[i]);
         }
     }
-    if ($('#twice-turn-over')[0].checked) {
-        var rows = result.split('\n');
-        result = '';
-        for (let i in rows) {
-            inBrackets = false;
-            let before;
-            let x = rows[i];
-            let newRow = '';
-            for (let j in x) {
-                let y = x[j];
-                if (y == '[' && missBrackets) {
-                    before = j;
-                    inBrackets = true;
-                } else if (y == ']' && missBrackets) {
-                    inBrackets = false;
-                    newRow = x.slice(before, parseInt(j) + 1) + newRow;
-                } else if (!inBrackets) {
-                    newRow =
-                        ((y == '(') ? ')' :
-                            (y == ')') ? '(' :
-                            (y == '（') ? '）' :
-                            (y == '）') ? '（' :
-                            (y == '{') ? '}' :
-                            (y == '}') ? '{' :
-                            (y == '《') ? '》' :
-                            (y == '》') ? '《' :
-                            (y == '<') ? '>' :
-                            (y == '>') ? '<' :
-                            (y == '【') ? '】' :
-                            (y == '】') ? '【' :
-                            y) +
-                        newRow;
-                }
+    return result;
+}
+WordsAway.prototype.turnOver = function (text, missBrackets = true) {
+    var rows = text.split('\n');
+    result = '';
+    for (let i in rows) {
+        inBrackets = false;
+        let before;
+        let x = rows[i];
+        let newRow = '';
+        for (let j in x) {
+            let y = x[j];
+            if (y == '[' && missBrackets) {
+                before = j;
+                inBrackets = true;
+            } else if (y == ']' && missBrackets) {
+                inBrackets = false;
+                newRow = x.slice(before, parseInt(j) + 1) + newRow;
+            } else if (!inBrackets) {
+                newRow =
+                    ((y == '(') ? ')' :
+                        (y == ')') ? '(' :
+                        (y == '（') ? '）' :
+                        (y == '）') ? '（' :
+                        (y == '{') ? '}' :
+                        (y == '}') ? '{' :
+                        (y == '《') ? '》' :
+                        (y == '》') ? '《' :
+                        (y == '<') ? '>' :
+                        (y == '>') ? '<' :
+                        (y == '【') ? '】' :
+                        (y == '】') ? '【' :
+                        y) +
+                    newRow;
             }
-            newRow = '\u202e' + newRow + '\n';
-            result += newRow;
         }
+        newRow = '\u202e' + newRow + '\n';
+        result += newRow;
     }
-    if (missBrackets) {
-        result = result.replace(/\[(http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?)\]/g, '$1');
-    }
-    $('pre.result').text(result);
-    $('.to-copy').attr('data-clipboard-text', result);
+    return result;
+}
+
+var wordsAway = new WordsAway();
+
+$('.start-mixin').click(function () {
+    var text = $('#textin').val();
+    var mixin = '\u200b';
+    var missBrackets = $('#miss-brackets')[0].checked;
+    text = (missBrackets) ? text.replace(/(http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?)/g, '[$1]') : text;
+    text = wordsAway.mixin(text, mixin, missBrackets);
+    text = ($('#twice-turn-over')[0].checked) ? wordsAway.turnOver(text, missBrackets) : text;
+    text = (missBrackets) ? text.replace(/\[(http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?)\]/g, '$1') : text;
+    $('pre.result').text(text);
+    $('.to-copy').attr('data-clipboard-text', text);
 })
+
 new ClipboardJS('.to-copy');
 $('.to-copy').click(function () {
     M.toast({
