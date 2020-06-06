fa7bacffa15ac0623518ea36d788c9d87ea69083
NitroRCr <havechicken666@gmail.com>
2020-06-06 22:49:55 +0800

    Update simple.js

--- js/simple.js
+++ js/simple.js
@@ -49,18 +49,17 @@ $('.start-mixin').click(function () {
             var settings = {
                 "async": true,
                 "crossDomain": true,
-                "url": "api/shortenurl/get?via=via&type=type&custom=custom&private=1&password=password&uses=100&url=https%253A%252F%252Fbaidu.com",
+                "url": "api/shortenurl/get?via=via&type=type&custom=custom&private=1&password=password&uses=100&url=" + encodeURIComponent(i),
                 "method": "GET",
                 "headers": {
                     "x-rapidapi-host": "free-url-shortener.p.rapidapi.com",
                     "x-rapidapi-key": "371c5214f2msh8192485aef7f824p176391jsn68ba5f579482",
                     "password": "",
-                    "custom": "",
+                    "custom": Math.random().toString(36).slice(-4),
                     "url": urls[i],
                     "private": ""
                 }
             }
-
             $.ajax(settings).done(function (response) {
                 console.log(response);
             });
