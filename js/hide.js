37e414f980ade2cee8e1bbd967a7346ac5454d90
NitroRCr <havechicken666@gmail.com>
2020-08-02 23:33:17 +0800

    update

--- js/hide.js
+++ js/hide.js
@@ -1,3 +1,67 @@
+function FromSeed(symbols, seed, length) {
+    this.symbols = symbols;
+    this.seed = seed;
+    this.length = length;
+    this.random = new Math.seedrandom(seed);
+    this.dictGroup = this.getDictGroup();
+    this.undictGroup = this.getUndictGroup();
+    this.order = this.getOrder();
+    this.unorder = this.getUnorder();
+    console.log(string2unicode(this.dictGroup[4][3]));
+}
+FromSeed.prototype.fakeShuffle = function (arr, random) {
+    var length = arr.length,
+        temp,
+        random_;
+    while (0 != length) {
+        random_ = Math.floor(random() * length);
+        length--;
+        temp = arr[length];
+        arr[length] = arr[random_];
+        arr[random_] = temp;
+    }
+    return arr;
+}
+FromSeed.prototype.getDict = function () {
+    var arr = this.fakeShuffle(Array.from(this.symbols), this.random).slice(0, 16);
+    var dict = {};
+    for (let i = 0; i < arr.length; i++) {
+        dict[i.toString(16)] = arr[i];
+    }
+    return dict;
+}
+FromSeed.prototype.getDictGroup = function () {
+    var dictGroup = [];
+    for (let i = 0; i < 16; i++) {
+        let dict = this.getDict();
+        dictGroup[i] = dict;
+    }
+    return dictGroup;
+}
+FromSeed.prototype.getUndictGroup = function () {
+    var undictGroup = [];
+    for (let i = 0; i < 16; i++) {
+        let undict = invertKeyValues(this.dictGroup[i])
+        undictGroup.push(undict);
+    }
+    return undictGroup;
+}
+FromSeed.prototype.getOrder = function () {
+    var order = {};
+    var arr = [];
+    for (let i = 0; i < this.length; i++) {
+        arr.push(i);
+    }
+    arr = this.fakeShuffle(arr, this.random);
+    for (let i = 0; i < this.length; i++) {
+        order[i] = arr[i];
+    }
+    return order;
+}
+FromSeed.prototype.getUnorder = function () {
+    return invertKeyValues(this.order);
+}
+
 function WordsDeep() {
     this.defaultSymbols = [
         '\u200b',
@@ -25,63 +89,58 @@ function WordsDeep() {
         '\u206e',
         '\u206f'
     ];
+    this.symbolsReg = /[\u200b-\u200f\u202a-\u202d\u2060-\u206f]+/;
 }
-WordsDeep.prototype.getDict = function (seed) {
-    var random = new Math.seedrandom(seed);
-    var symbols = this.defaultSymbols;
-    var count = 16;
-    var shuffled = symbols.slice(0),
-        i = symbols.length,
-        min = i - count,
-        temp, index;
-    while (i-- > min) {
-        index = Math.floor((i + 1) * random());
-        temp = shuffled[index];
-        shuffled[index] = shuffled[i];
-        shuffled[i] = temp;
-    }
-    var array = shuffled.slice(min);
-    var dict = {}
-    for (let i = 0; i < count; i++) {
-        dict[i.toString(16)] = array[i];
-    }
-    return dict;
-}
-WordsDeep.prototype.hide = function (text, password) {
-    var dict = this.getDict(password);
-    var mark = '\u202d';
-    var result = '';
-    for (let i in text) {
-        result += mark;
-        let code16 = text.charCodeAt(i).toString(16);
+WordsDeep.prototype.hide = function (binary, password) {
+    var fromSeed = new FromSeed(this.defaultSymbols, password, binary.length * 2);
+    console.log(fromSeed);
+    var first = '';
+    for (let i in binary) {
+        let code16 = binary.charCodeAt(i).toString(16);
+        if (code16.length < 2) {
+            code16 = '0' + code16;
+        }
         for (let j of code16) {
-            result += dict[j];
+            first += fromSeed.dictGroup[i%16][j];
         }
     }
-    return result;
-}
-WordsDeep.prototype.unhide = function (text, password) {
-    var undict = this.invertKeyValues(this.getDict(password));
-    var mark = '\u202d';
-    var result = '';
-    for (var i = 0; i < text.length;) {
-        if (text[i] == mark) {
-            let char = '';
-            while (text[++i] && text[i] != mark) {
-                char += undict[text[i]];
-            }
-            result += String.fromCharCode(parseInt(char, 16));
-        } else {
-            result += text[i++];
-        }
+    var second = [];
+    for (let i in fromSeed.order) {
+        second[i] = first[fromSeed.order[i]];
     }
-    return result
+    return second.join("");
 }
-WordsDeep.prototype.invertKeyValues = obj =>
+WordsDeep.prototype.unhide = function (hidden, password) {
+    var fromSeed = new FromSeed(this.defaultSymbols, password, hidden.length);
+    console.log(fromSeed);
+    var unordered = [];
+    for (let i in fromSeed.unorder) {
+        unordered[i] = hidden[fromSeed.unorder[i]];
+    }
+    unordered = unordered.join('');
+    var binary = ''
+    for (let i = 0; i < unordered.length; i += 2) {
+        let undict = fromSeed.undictGroup[(i/2)%16];
+        let code16 = undict[unordered[i]] + undict[unordered[i + 1]];
+        binary += String.fromCharCode(parseInt(code16, 16));
+    }
+    return binary;
+}
+const invertKeyValues = obj =>
     Object.keys(obj).reduce((acc, key) => {
         acc[obj[key]] = key;
         return acc;
     }, {});
+WordsDeep.prototype.compress = function (text) {
+    return pako.gzip(text, {
+        to: 'string'
+    });
+}
+WordsDeep.prototype.uncompress = function (binary) {
+    return pako.ungzip(binary, {
+        to: 'string'
+    });
+}
 
 new ClipboardJS('.to-copy');
 $('.to-copy').click(function () {
@@ -90,7 +149,7 @@ $('.to-copy').click(function () {
     });
 });
 
-var wordsDeep = new WordsDeep();
+var wd = new WordsDeep();
 $('.start-mixin').click(function () {
     var text = $('#textin').val();
     if ($('#if-encrypt')[0].checked) {
@@ -98,18 +157,75 @@ $('.start-mixin').click(function () {
     } else {
         var password = '';
     }
-    var result = ($('#back-mode')[0].checked) ?
-        wordsDeep.unhide(text, password) :
-        wordsDeep.hide(text, password);
-    result = ' ' + result + ' ';
+    var back = $('#back-mode')[0].checked;
+    var ifCompress = $('#if-compress')[0].checked;
+    if (back) {
+        let hiddenStr = text.match(wd.symbolsReg)[0];
+        if (ifCompress) {
+            let binaryStr = wd.unhide(hiddenStr, password);
+            console.log(binaryStr);
+            var factStr = wd.uncompress(binaryStr);
+        } else {
+            var factStr = wd.unhide(hiddenStr, password);
+        }
+        text = text.replace(hiddenStr, factStr);
+    } else {
+        if (ifCompress) {
+            text = wd.compress(text);
+        };
+        console.log('compressed: ' + text);
+        text = wd.hide(text, password);
+    }
+    result = ' ' + text + ' ';
     $('pre.result').text(result);
     $('.to-copy').attr('data-clipboard-text', result);
 });
 
-$('#if-encrypt').click(function() {
-    if($(this)[0].checked) {
+$('#if-encrypt').click(function () {
+    if ($(this)[0].checked) {
         $('div.password').css('display', 'block');
     } else {
         $('div.password').css('display', 'none');
     }
 });
+
+function string2unicode(text, mode2 = false, filter) {
+    var result = '';
+    if (filter) {
+        var reg = new RegExp('[' + filter + ']');
+    }
+    if (mode2) {
+        var list = Array.from(text);
+        for (let i of list) {
+            if (filter && reg.test(i)) {
+                result += i;
+            } else {
+                result += ustr(i.codePointAt(0));
+            }
+        }
+    } else {
+        for (let i in text) {
+            if (filter && reg.test(text[i])) {
+                result += text[i];
+            } else {
+                result += ustr(text.charCodeAt(i));
+            }
+        }
+    }
+    return result;
+}
+
+function ustr(code) {
+    var code16 = code.toString(16);
+    if (code < 0xf) {
+        return "\\u" + "000" + code16;
+    } else if (code <= 0xff) {
+        return "\\u" + "00" + code16;
+    } else if (code <= 0xfff) {
+        return "\\u" + "0" + code16;
+    } else if (code <= 0xffff) {
+        return "\\u" + code16;
+    } else {
+        return "\\u{" + code16 + "}";
+    }
+}
